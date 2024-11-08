c34e3d9a1774e38ca993c4b49f19f15ad0be3811
Kittipong Tapyou <kittipong.tpy@gmail.com>
2024-11-08 19:39:01 +0700

    move postPath to protected

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -100,7 +100,7 @@ export const Sidebar = () => {
                                                     }),
                                                     'data-[active=true]:text-primary data-[active=true]:font-medium cursor-pointer',
                                                     currentPath ===
-                                                        `/workspace/${event._id}` &&
+                                                        `/protected/workspace/${event._id}` &&
                                                         'text-violet-500 bg-default-100',
                                                     'flex items-center gap-2 p-2 rounded-lg hover:bg-default-100 dark:hover:bg-default-800 dark:hover:text-default-100',
                                                 )}
@@ -108,7 +108,7 @@ export const Sidebar = () => {
                                                     const eventID = event._id;
 
                                                     navigate(
-                                                        `/workspace/${eventID}`,
+                                                        `/protected/workspace/${eventID}`,
                                                         {
                                                             state: { event },
                                                         },
